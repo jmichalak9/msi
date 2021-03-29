90643d97b3195d51338463bd6066805dd8929a91
Jakub Michalak <michalakjakub1999@gmail.com>
2021-03-29 12:17:24 +0200

    Add fuzzy implications

---
+++ FuzzySetLib/Implications.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzySetLib
+{
+    public class Implications
+    {
+        public static float Godel(float x, float y)
+        {
+            CheckImplArguments(x, y);
+            if (x <= y)
+                return 1;
+            return y;
+        }
+        public static float KleeneDienes(float x, float y)
+        {
+            CheckImplArguments(x, y);
+            return Math.Max(1 - x, y);
+        }
+
+        public static float Lukasiewicz(float x, float y)
+        {
+            CheckImplArguments(x, y);
+            return Math.Min(1, 1 - x + y);
+        }
+
+        private static void CheckImplArguments(float x, float y)
+        {
+            if (x < 0 || x > 1)
+                throw new ArgumentException($"invalid x value: {x}");
+            if (y < 0 || y > 1)
+                throw new ArgumentException($"invalid y value: {y}");
+        }
+    }
+}
