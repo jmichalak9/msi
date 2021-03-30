1853697ccadb4d8da53347f206c45b41c89d5a84
maxization <damian.opoka99@gmail.com>
2021-03-30 17:17:44 +0200

    Add norms

---
+++ FuzzySetLib/Norms.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzySetLib
+{
+    public class Norms
+    {
+        public static float Standard(float x, float y)
+        {
+            CheckNormArguments(x, y);
+            return Math.Min(x, y);
+        }
+
+        public static float Product(float x, float y)
+        {
+            CheckNormArguments(x, y);
+            return x * y;
+        }
+
+        public static float Lukasiewicz(float x, float y)
+        {
+            CheckNormArguments(x, y);
+            return Math.Max(0, x + y - 1);
+        }
+
+        private static void CheckNormArguments(float x, float y)
+        {
+            if (x < 0 || x > 1)
+                throw new ArgumentException($"invalid x value: {x}");
+            if (y < 0 || y > 1)
+                throw new ArgumentException($"invalid y value: {y}");
+        }
+    }
+}
---
+++ FuzzySetLibTests/NormsTest.cs
@@ -0,0 +1,50 @@
+﻿using FuzzySetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FuzzySetLibTests
+{
+    public class NormsTest
+    {
+        [Fact]
+        public void StandardTest()
+        {
+            AssertNaturalNorm(Norms.Standard);
+        }
+
+        [Fact]
+        public void ProductTest()
+        {
+            AssertNaturalNorm(Norms.Product);
+        }
+
+        [Fact]
+        public void LukasiewiczTest()
+        {
+            AssertNaturalNorm(Norms.Lukasiewicz);
+        }
+
+        private static void AssertNaturalNorm(Func<float, float, float> norm)
+        {
+            float x = 0.2f;
+            float y = 0.5f;
+            float z = 0.7f;
+            int precision = 4;
+
+            //T-norm conditions
+            Assert.Equal(norm(x, y), norm(y, x));
+            Assert.Equal(norm(x, norm(y, z)), norm(norm(x, y), z));
+            Assert.True(norm(x, y) <= norm(z, y));
+            Assert.Equal(x, norm(x, 1), precision);
+
+            Assert.Throws<ArgumentException>(() => norm(-1, 0));
+            Assert.Throws<ArgumentException>(() => norm(2, 0));
+            Assert.Throws<ArgumentException>(() => norm(0, -1));
+            Assert.Throws<ArgumentException>(() => norm(0, 2));
+        }
+    }
+}
