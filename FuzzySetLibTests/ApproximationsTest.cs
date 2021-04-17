8b676ff5ce9b27d21eaacf1ad585929096626fb3
Jakub Michalak <michalakjakub1999@gmail.com>
2021-04-17 21:10:39 +0200

    Add second step (#7)

--- FuzzySetLibTests/ApproximationsTest.cs
+++ FuzzySetLibTests/ApproximationsTest.cs
@@ -3,11 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace FuzzySetLibTests
 {
     public class ApproximationsTest
-    {
+    {    
+        private readonly ITestOutputHelper _testOutputHelper;
+        public ApproximationsTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
         [Fact]
         public void ObjectBoundApproximationTest()
         {
@@ -33,6 +39,36 @@ namespace FuzzySetLibTests
             };
 
             var act = Approximations.ObjectBoundApproximation(Norms.Lukasiewicz, Implications.Lukasiewicz, Q, R);
+
+            AssertApproximationsEqual(exp, act);
+        }
+        
+        [Fact]
+        public void PropertyBoundApproximationTest()
+        {
+            var Q = new float[,]{
+                { 0.4f, 0.3f, 0.1f, 0.4f, 0.1f },
+                { 0.7f, 0.1f, 0.0f, 0.7f, 0.1f },
+                { 0.3f, 0.6f, 0.2f, 0.2f, 0.1f },
+                { 0.1f, 0.2f, 0.8f, 0.2f, 0.2f },
+                { 0.1f, 0.0f, 0.2f, 0.2f, 0.8f },
+            };
+            var R = new float[,]{
+                { 0.8f, 0.6f, 0.2f, 0.6f, 0.1f },
+                { 0.0f, 0.4f, 0.6f, 0.1f, 0.1f },
+                { 0.8f, 0.8f, 0.0f, 0.2f, 0.0f },
+                { 0.6f, 0.5f, 0.3f, 0.7f, 0.3f },
+            };
+            var exp = new Bounds[,]
+            {
+                { b(0.4,0.4), b(0.3,0.4), b(0.1,0.2), b(0.4,0.4), b(0.1,0.2), },
+                { b(0.7,0.7), b(0.1,0.4), b(0.0,0.2), b(0.7,0.7), b(0.1,0.2), },
+                { b(0.3,0.3), b(0.6,0.6), b(0.2,0.2), b(0.2,0.3), b(0.1,0.2), },
+                { b(0.1,0.3), b(0.2,0.4), b(0.8,0.8), b(0.2,0.3), b(0.2,0.2), },
+                { b(0.1,0.3), b(0.0,0.4), b(0.2,0.2), b(0.2,0.3), b(0.8,0.8), },
+            };
+
+            var act = Approximations.PropertyBoundApproximation(Norms.Lukasiewicz, Implications.Lukasiewicz, Q, R);
 
             AssertApproximationsEqual(exp, act);
         }
