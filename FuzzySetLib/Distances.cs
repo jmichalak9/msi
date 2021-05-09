20beff9576e55bd17b6b2784bf427f67fe6a4588
maxization <damian.opoka99@gmail.com>
2021-05-09 20:29:59 +0200

    Add distances with tests

--- FuzzySetLib/Distances.cs
+++ FuzzySetLib/Distances.cs
@@ -8,30 +8,56 @@ namespace FuzzySetLib
 {
     public class Distances
     {
-        public static float[,] CalculateDistance(Func<Bounds,Bounds, float> distance, Func<float, float, float> norm, Func<float, float, float> impl, float[,] Q, float[,] R)
+        public static float[,] HammingDistance(Func<float, float, float> norm, Func<float, float, float> impl, float[,] Q, float[,] R)
         {
-            float[,] distances = new float[R.GetLength(0), R.GetLength(1)];
+            float[,] distances = new float[Q.GetLength(0), R.GetLength(0)];
 
             var objectBounds = Approximations.ObjectBoundApproximation(norm, impl, Q, R);
             var propertyBounds = Approximations.PropertyBoundApproximation(norm, impl, Q, R);
 
             for(int i=0;i<distances.GetLength(0);i++)
             {
-                for(int j=0;i<distances.GetLength(1);j++)
+                for(int j=0;j<distances.GetLength(1);j++)
                 {
-                    distances[i, j] = distance(objectBounds[i, j], propertyBounds[i, j]);
+                    for(int k=0;k<objectBounds.GetLength(1);k++)
+                    {
+                        distances[i, j] += HammingDistance(propertyBounds[i, k], objectBounds[j, k]);
+                    }
+                    distances[i, j] /= 2 * objectBounds.GetLength(1);
                 }
             }
 
             return distances;
         }
 
-        public float HammingDistance(Bounds A, Bounds B)
+        public static float[,] EuclideanDistance(Func<float, float, float> norm, Func<float, float, float> impl, float[,] Q, float[,] R)
+        {
+            float[,] distances = new float[Q.GetLength(0), R.GetLength(0)];
+
+            var objectBounds = Approximations.ObjectBoundApproximation(norm, impl, Q, R);
+            var propertyBounds = Approximations.PropertyBoundApproximation(norm, impl, Q, R);
+
+            for (int i = 0; i < distances.GetLength(0); i++)
+            {
+                for (int j = 0; j < distances.GetLength(1); j++)
+                {
+                    for (int k = 0; k < objectBounds.GetLength(1); k++)
+                    {
+                        distances[i, j] += EuclideanDistance(propertyBounds[i, k], objectBounds[j, k]);
+                    }
+                    distances[i, j] = (float)Math.Sqrt(distances[i, j] / (2 * objectBounds.GetLength(1)));
+                }
+            }
+
+            return distances;
+        }
+
+        private static float HammingDistance(Bounds A, Bounds B)
         {
             return Math.Abs(A.upper - B.upper) + Math.Abs(A.lower - B.lower);
         }
 
-        public float EuclideanDistance(Bounds A, Bounds B)
+        private static float EuclideanDistance(Bounds A, Bounds B)
         {
             return (A.upper - B.upper) * (A.upper - B.upper) + (A.lower - B.lower) * (A.lower - B.lower);
         }
--- FuzzySetLibTests/DistancesTest.cs
+++ FuzzySetLibTests/DistancesTest.cs
@@ -1,19 +1,28 @@
-﻿using System;
+﻿using FuzzySetLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace FuzzySetLibTests
 {
     public class DistancesTest
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly int _precision = 2;
+        public DistancesTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
 
         [Fact]
         public void HammingDistanceTest()
         {
-            var Q = new float[,]{
+            var Q = new float[,]
+            {
                   { 0.4f, 0.3f, 0.1f, 0.4f, 0.1f },
                   { 0.7f, 0.1f, 0.0f, 0.7f, 0.1f },
                   { 0.3f, 0.6f, 0.2f, 0.2f, 0.1f },
@@ -21,13 +30,73 @@ namespace FuzzySetLibTests
                   { 0.1f, 0.0f, 0.2f, 0.2f, 0.8f },
             };
 
-            var R = new float[,]{
+            var R = new float[,]
+            {
                   { 0.8f, 0.6f, 0.2f, 0.6f, 0.1f },
                   { 0.0f, 0.4f, 0.6f, 0.1f, 0.1f },
                   { 0.8f, 0.8f, 0.0f, 0.2f, 0.0f },
                   { 0.6f, 0.5f, 0.3f, 0.7f, 0.3f },
             };
 
+            var exp = new float[,]
+            {
+                {0.18f, 0.19f, 0.19f, 0.19f },
+                {0.13f, 0.34f, 0.2f, 0.14f  },
+                {0.17f, 0.18f, 0.18f, 0.22f },
+                {0.38f, 0.09f, 0.33f, 0.33f },
+                {0.40f, 0.27f, 0.35f, 0.35f }
+            };
+
+            var result = Distances.HammingDistance(Norms.Lukasiewicz, Implications.Lukasiewicz, Q, R);
+
+            for(int i=0;i<result.GetLength(0);i++)
+            {
+                for(int j=0;j<result.GetLength(1);j++)
+                {
+                    Assert.Equal(exp[i, j], result[i, j], _precision);
+                }
+            }
+
+        }
+
+        [Fact]
+        public void EuclideanDistance()
+        {
+            var Q = new float[,]
+            {
+                  { 0.4f, 0.3f, 0.1f, 0.4f, 0.1f },
+                  { 0.7f, 0.1f, 0.0f, 0.7f, 0.1f },
+                  { 0.3f, 0.6f, 0.2f, 0.2f, 0.1f },
+                  { 0.1f, 0.2f, 0.8f, 0.2f, 0.2f },
+                  { 0.1f, 0.0f, 0.2f, 0.2f, 0.8f },
+            };
+
+            var R = new float[,]
+            {
+                  { 0.8f, 0.6f, 0.2f, 0.6f, 0.1f },
+                  { 0.0f, 0.4f, 0.6f, 0.1f, 0.1f },
+                  { 0.8f, 0.8f, 0.0f, 0.2f, 0.0f },
+                  { 0.6f, 0.5f, 0.3f, 0.7f, 0.3f },
+            };
+
+            var exp = new float[,]
+            {
+                { 0.23f, 0.27f, 0.24f, 0.20f },
+                { 0.19f, 0.43f, 0.28f, 0.18f },
+                { 0.27f, 0.24f, 0.25f, 0.26f },
+                { 0.43f, 0.12f, 0.42f, 0.37f },
+                { 0.46f, 0.36f, 0.43f, 0.39f }
+            };
+
+            var result = Distances.EuclideanDistance(Norms.Lukasiewicz, Implications.Lukasiewicz, Q, R);
+
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Assert.Equal(exp[i, j], result[i, j], _precision);
+                }
+            }
         }
     }
 }
